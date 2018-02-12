21dce2ce6eede3e16f52b35b8ffb576a5bbc1bb2
Brigham Holland <34488916+117529@users.noreply.github.com>
2018-02-11 22:18:18 -0700

    Add files via upload

---
+++ Daily Exercise 02082018 num3 (2).py
@@ -0,0 +1,45 @@
+#Brigham Holland - Python Daily exercise 3 - 02082018
+for i in range(21):
+        if (i == 0):
+                print ('1')
+        elif (i == 1):
+                print ('2')
+        elif (i == 2):
+                print ('usb')
+        elif (i == 3):
+                print ('4')
+        elif (i == 4):
+                print ('device')
+        elif (i == 5):
+                print ('usb')
+        elif (i == 6):
+                print ('7')
+        elif (i == 7):
+                print ('8')
+        elif (i == 8):
+                print ('usb')
+        elif (i == 9):
+                print ('device')
+        elif (i == 10):
+                print ('11')
+        elif (i == 11):
+                print ('usb')
+        elif (i == 12):
+                print ('13')
+        elif (i == 13):
+                print ('14')
+        elif (i == 14):
+                print ('usb device')
+        elif (i == 15):
+                print ('16')
+        elif (i == 16):
+                print ('17')
+        elif (i == 17):
+                print ('usb')
+        elif (i == 18):
+                print ('19')
+        elif (i == 19):
+                print ('device')
+        else:
+                
+                exit()
---
+++ Daily exercise 02282018 num2.py
@@ -0,0 +1,5 @@
+#Brighamm Holland - Python Daily exercise 2 - 02082018
+name = input('What is your name? ')
+age = int(input('How old are you? '))
+onehundred = int(100 - age)
+print('You will be 100 in ' + str(2018 + onehundred) + ' years')
